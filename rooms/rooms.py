c5cf3f7c9dee2e25f3c7fe2b85a2c2f256da397b
Eddy <eddy@localhost.localdomain>
2016-10-12 14:59:03 +0300

    FT: Adds create room method

--- rooms/rooms.py
+++ rooms/rooms.py
@@ -1,23 +1,45 @@
-from amity.amity import Amity
+# from amity.amity import Amity
 
-offices = []
-living_spaces = []
-
-
-class Room(Amity):
+rooms_list = []
+class Room(object):
 	"""Super class for rooms. Defines methods that affect
 	both office	and living space classes.
 	"""
-	def __init__(self, name):
-		self.name = name
-		if not self.name.isalpha():
-			return("Error. Name can only contains alphabets")		
+	def __init__(self, r_type=None, capacity=None):
+		self.r_type = r_type
+		self.capacity = capacity
 
-	def print_allocations(self):
-		#return all the allocations; ie all rooms and people
-		#in them.
-		pass
+	def create_room(self,room_name):
+		room_dict = dict()
+		room_dict["Name"] = room_name
+		room_dict["Type"] = self.r_type
+		room_dict["Capacity"] = self.capacity
 
-	def print_room(self):
-		pass
+		rooms_list.append(room_dict)
+		# rooms_list.append(room_name)
 		
+	def print_allocations(self):
+		"""Prints all rooms and all the people that have been allocated 
+		those rooms
+		"""
+		pass
+
+	def print_unallocated(self):
+		pass
+
+
+class Office(Room):
+	"""Performs tasks unique to offices
+	"""
+	def __init__(self, name):
+		super(Office, self).__init__(r_type="OFFICE", capacity=6)
+
+class LivingSpace(Room):
+	"""Docstring
+	"""
+	def __init__(self):
+		super(LivingSpace, self).__init__(r_type="LIVINGSPACE", capacity=4)
+
+l = LivingSpace()
+l.create_room('Krypton')
+print(rooms_list)
