9680bcb83dd799ee3f2489741aca0823b4e47b95
Eddy <eddy@localhost.localdomain>
2016-10-14 17:46:01 +0300

    Commit

---
+++ sample.py
@@ -0,0 +1,24 @@
+from tabulate import tabulate
+from rooms import LivingSpace, Office
+
+d = [
+	{
+		'Capacity': 4, 
+		'Type': 'LIVINGSPACE', 
+		'Name': 'Krypton'
+	}, 
+	{
+		'Capacity': 4, 
+		'Type': 'LIVINGSPACE', 
+		'Name': 'Jail'
+	}, 
+	{
+		'Capacity': 6, 
+		'Type': 'OFFICE', 
+		'Name': 'Krypton'
+	}
+]
+h = (d[0].keys())
+print(h)
+
+print(tabulate(d, headers="keys"))
